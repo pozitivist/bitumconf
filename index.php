8b7801cacd25863c394bb9ededffb33ee17e9e54
Mac <Mac@MacBook-Pro-3.local>
2019-02-04 10:57:04 +0300

    Firdst commit

---
+++ index.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Международная конференция PRO БИТУМ И ПБВ</title>
+  <link rel="stylesheet" type= "text/css" href="css/style.css">
+  <link rel="stylesheet" type= "text/css" href="fonts/style.css">
+  <meta http-equiv="Content-Type">
+</head>
+<body>
+
+<div class="container">
+  <header>
+     <div class="header-row">
+       <div class="col">
+         <a href="#"><img class="logo" src="i/logo.svg" alt="Международная конференция PRO БИТУМ И ПБВ" /></a>
+       </div>
+       <div class="col title">PRO БИТУМ И ПБВ. САНКТ-ПЕТЕРБУРГ <span class="orange">4-5 АПРЕЛЯ 2019</span></div>
+       <div class="col reg"><a class="btn" href="#">Регистрация</a></div>
+     </div>
+  </header>
+
+  <!--<nav></nav>-->
+  <div class="slider">
+    <div class="slider-row">
+      <div class="col">
+        <img src="i/banner_1.jpg" alt="PRO БИТУМ И ПБВ САНКТ-ПЕТЕРБУРГ 4-5 АПРЕЛЯ 2019" />
+      </div>
+    </div>
+  </div>
+
+  <main>
+    <div class="main-row">
+      <div class="col img_1"></div>
+      <div class="col hex_1">
+        <div class="title">О КОНФЕРЕНЦИИ</div>
+        <p>За эти годы конференция стала единой отраслевой площадкой и крупнейшим межотраслевым мероприятием по вопросам производства и применения битумных и битумопроизводных материалов в российском дорожном строительстве, а также в аэропортовой инфраструктуре.</p>
+        <p>Участниками сессий и дискуссий ежегодно становятся свыше 400 представителей федеральных органов исполнительной власти, ведущих подрядных и проектных организаций, производителей дорожных битумов и полимерно-битумных вяжущих (ПБВ).</p>
+
+      </div>
+
+      <div class="col hex_2">
+        <div class="title">ДАТА И МЕСТО ПРОВЕДЕНИЯ</div>
+        <p>За эти годы конференция стала единой отраслевой площадкой и крупнейшим межотраслевым мероприятием по вопросам производства и применения битумных и битумопроизводных материалов в российском дорожном строительстве, а также в аэропортовой инфраструктуре.</p>
+        <a href="#" class="btn m2">Регистрация</a>
+      </div>
+      <div class="col img_2"></div>
+
+      <div class="col img_3"></div>
+      <div class="col hex_3">
+        <div class="title">ИНФОРМАЦИЯ О ПЛОЩАДКЕ, УСЛОВИЯХ ПРОЖИВАНИЯ</div>
+        <p>За эти годы конференция стала единой отраслевой площадкой и крупнейшим межотраслевым мероприятием по вопросам производства и применения битумных и битумопроизводных материалов в российском дорожном строительстве, а также в аэропортовой инфраструктуре.</p>
+        <a href="#" class="btn m3">www.corinthia.com</a>
+      </div>
+
+      <div class="col hex_4">
+        <div class="title">СПИКЕРЫ</div>
+        <p>За эти годы конференция стала единой отраслевой площадкой и крупнейшим межотраслевым мероприятием по вопросам производства и применения битумных и битумопроизводных материалов в российском дорожном строительстве, а также в аэропортовой инфраструктуре.</p>
+
+      </div>
+      <div class="col img_4"></div>
+
+      <div class="col img_5"></div>
+      <div class="col bg-gray">
+        <div class="title">ПАРТНЕРЫ</div>
+        <ul class="partners">
+          <li><img src="i/gazprom.svg" alt="Газпром" /></li>
+          <li><img src="i/sibur.svg" alt="Сибур" /></li>
+          <li><img src="i/rosneft-bitum.svg" alt="Роснефть Битум" /></li>
+          <li><img src="i/rosavtodor.svg" alt="Росавтодор" /></li>
+          <li><img src="i/avtodor.svg" alt="Автодор" /></li>
+        </ul>
+      </div>
+    </div>
+  </main>
+
+  <footer>
+    <div class="footer-row">
+      <div class="col"><a href="#"><img class="logo_footer" src="i/logo.svg" alt="Международная конференция PRO БИТУМ И ПБВ" /></a></div>
+      <div class="col copyright">
+        <a href="#">Правовая информация</a><br />
+        © 2019 Все права защищены
+      </div>
+      <div class="col"><!--Меню--></div>
+    </div>
+  </footer>
+</div>
+
+</body>
+</html>
